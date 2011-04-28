6ed625219ccb6094b10accc7bdb612404150db01
Nelson DaSilva <wrcsti@gmail.com>
2011-04-28 15:31:09 -0400

    initial commit

---
+++ Classes/RootViewController.h
@@ -0,0 +1,14 @@
+//
+//  RootViewController.h
+//  Weather_Map
+//
+//  Created by Nelson DaSilva on 4/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+}
+
+@end
---
+++ Classes/Weather_MapAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  Weather_MapAppDelegate.h
+//  Weather_Map
+//
+//  Created by Nelson DaSilva on 4/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Weather_MapAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
+
